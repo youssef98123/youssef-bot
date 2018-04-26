5c6bd9ca25c43e5e0ff58ea53e29051eecb95b49
youssef98123 <35854564+youssef98123@users.noreply.github.com>
2018-04-27 00:25:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,30 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
+client.on('message', message => {
+   let embed = new Discord.RichEmbed()
+    let args = message.content.split(' ').slice(1).join(' ');
+     if(!message.channel.guild) return;
+     if (message.content.startsWith(prefix + "bc")) {
+      if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply('*** You Dont Have Permission to do that ***');
+         message.react("✔️")
+          let embed = new Discord.RichEmbed()
+          .setColor("#FF00FF")
+          .setThumbnail(message.author.avatarURL)   
+          .addField('Done By:', "<@" + message.author.id + ">")
+
+           message.channel.sendEmbed(embed);
+            message.guild.members.forEach(m => {
+            var bc = new Discord.RichEmbed()
+          .addField('**❁Server name **', `*** ⇝ ${message.guild.name}***`)               
+          .addField(`***❁message***`, args)
+          .setColor('#B40486')
+          .addField('**❁Name of sender**', `*** ⇝ ${message.author.username}#${message.author.discriminator}***`)
+         m.send(``,{embed: bc});
+        });
+        
+    }
+    
+});
+
+client.login('token');
